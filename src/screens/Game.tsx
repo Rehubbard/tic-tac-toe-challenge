6adcab30d084268804549fb05459e737333a285f
Eric Hubbard <rehubbard2@gmail.com>
2019-07-24 11:22:18 -0500

    Basic board layout

--- src/screens/Game.tsx
+++ src/screens/Game.tsx
@@ -1,28 +1,97 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaView, NavigationScreenProps } from "react-navigation";
 
-import { Text } from '../components'
+import { Text } from "../components";
+import globalColors from "../styles/globalColors";
+import {
+  BOARD_HEIGHT,
+  BOARD_WIDTH,
+  SQUARE_WIDTH,
+  SQUARE_HEIGHT
+} from "../styles/globalStyles";
 
-type Props = {
-
-} & NavigationScreenProps
+type Props = {} & NavigationScreenProps;
 
 class Game extends React.Component<Props> {
-
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <Text type='bold'>Game Screen</Text>
+        <View style={styles.board}>
+          <View style={[styles.verticalBoardLine, styles.firstVertical]} />
+          <View style={[styles.verticalBoardLine, styles.secondVertical]} />
+          <View style={styles.square}>
+            <Text>X</Text>
+          </View>
+          <View style={styles.square}>
+            <Text>X</Text>
+          </View>
+          <View style={styles.square}>
+            <Text>X</Text>
+          </View>
+          <View style={styles.boardLine} />
+          <View style={styles.square}>
+            <Text>X</Text>
+          </View>
+          <View style={styles.square}>
+            <Text>X</Text>
+          </View>
+          <View style={styles.square}>
+            <Text>X</Text>
+          </View>
+          <View style={styles.boardLine} />
+          <View style={styles.square}>
+            <Text>X</Text>
+          </View>
+          <View style={styles.square}>
+            <Text>X</Text>
+          </View>
+          <View style={styles.square}>
+            <Text>X</Text>
+          </View>
+        </View>
       </SafeAreaView>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  board: {
+    height: BOARD_HEIGHT,
+    width: BOARD_WIDTH,
+    flexDirection: "row",
+    flexWrap: "wrap"
+  },
+  square: {
+    width: SQUARE_WIDTH,
+    height: SQUARE_HEIGHT,
+    alignSelf: "stretch",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10
+  },
+  boardLine: {
+    height: 2,
+    width: BOARD_WIDTH,
+    backgroundColor: globalColors.black
+  },
+  verticalBoardLine: {
+    position: "absolute",
+    height: BOARD_HEIGHT,
+    width: 2,
+    backgroundColor: globalColors.black
+  },
+  firstVertical: {
+    transform: [{ translateX: BOARD_WIDTH / 3 }]
+  },
+  secondVertical: {
+    transform: [{ translateX: (BOARD_WIDTH / 3) * 2 }]
   }
-})
+});
 
-export default Game
+export default Game;
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,4 @@
+export const BOARD_HEIGHT = 350;
+export const BOARD_WIDTH = 350;
+export const SQUARE_HEIGHT = BOARD_HEIGHT / 3;
+export const SQUARE_WIDTH = BOARD_WIDTH / 3;

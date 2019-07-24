f1de2fa2d9ffc11fd1af4e29298e5e322a7fb147
Eric Hubbard <rehubbard2@gmail.com>
2019-07-23 23:40:39 -0500

    react-native-vector-icons installed and linked
    Working on iOS

--- src/components/HeaderRight.tsx
+++ src/components/HeaderRight.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { withNavigation, NavigationInjectedProps } from "react-navigation";
 
 import Text from './Text';
 
-type Props = { }
+type Props = { } & NavigationInjectedProps
 
 const HeaderRight: React.FunctionComponent<Props> = (props) => 
-  <View>
-    <Text>Header Right</Text>
-  </View>
+<TouchableOpacity
+onPress={() => props.navigation.toggleDrawer()}
+style={{ marginHorizontal: 10 }}
+hitSlop={{ top: 20, left: 20, bottom: 20, right: 20 }}
+>
+    <Icon name='bars' color='black' size={18} />
+  </TouchableOpacity>
 
-export default HeaderRight
+export default withNavigation(HeaderRight)

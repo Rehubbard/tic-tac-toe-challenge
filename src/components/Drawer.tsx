d7f0a4c322cc054c97810f823aa80b246d418a70
Eric Hubbard <rehubbard2@gmail.com>
2019-07-24 10:44:07 -0500

    Basic drawer styles
    Also added some colors to globalColors

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,29 +1,59 @@
 import React from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
-import { SafeAreaView } from "react-navigation";
-import { Icon } from "react-native-vector-icons/Icon";
+import {
+  SafeAreaView,
+  NavigationScreenProps,
+  DrawerItemsProps
+} from "react-navigation";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 import { Text } from ".";
 import globalColors from "../styles/globalColors";
 
-interface Props {}
+type Props = {} & NavigationScreenProps & DrawerItemsProps;
 
 class Drawer extends React.Component<Props> {
+  navigateAndCloseDrawer = (route: string) => {
+    this.props.navigation.toggleDrawer();
+    this.props.navigation.navigate(route);
+  };
+
   render() {
+    console.log(this.props.activeItemKey);
     return (
       <SafeAreaView style={styles.container} forceInset={{ top: "always" }}>
-        <TouchableOpacity style={styles.drawerItem} onPress={onPress}>
-          <Text style={styles.drawerLink}>{label}</Text>
-          {hasArrow && (
+        <TouchableOpacity
+          style={styles.drawerItem}
+          onPress={() => this.navigateAndCloseDrawer("GameStack")}
+        >
+          {this.props.activeItemKey === "GameStack" && (
             <Icon
-              name="chevron-right"
-              color={globalColors.blue}
-              iconStyle={styles.arrow}
-              size={14}
+              name="circle"
+              color={globalColors.lightGreen}
+              size={10}
+              style={styles.activeIndicator}
             />
           )}
+          <Text style={styles.drawerLink} type="bold">
+            Play
+          </Text>
         </TouchableOpacity>
-        <View style={styles.drawerItem}>Stats</View>
+        <TouchableOpacity
+          style={styles.drawerItem}
+          onPress={() => this.navigateAndCloseDrawer("StatsStack")}
+        >
+          {this.props.activeItemKey === "StatsStack" && (
+            <Icon
+              name="circle"
+              color={globalColors.lightGreen}
+              size={10}
+              style={styles.activeIndicator}
+            />
+          )}
+          <Text style={styles.drawerLink} type="bold">
+            Stats
+          </Text>
+        </TouchableOpacity>
       </SafeAreaView>
     );
   }
@@ -31,12 +61,22 @@ class Drawer extends React.Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: globalColors.green,
+    paddingHorizontal: 20
   },
   drawerItem: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between"
+    justifyContent: "flex-end",
+    marginVertical: 30
+  },
+  drawerLink: {
+    fontSize: 32,
+    color: globalColors.white
+  },
+  activeIndicator: {
+    marginRight: 10
   }
 });
 
--- src/styles/globalColors.ts
+++ src/styles/globalColors.ts
@@ -1,5 +1,7 @@
 export default {
   green: "#66b249",
+  darkGreen: "#315C36",
+  lightGreen: "#9be358",
   black: "#343434",
   grey: "#636363",
   white: "#FFFFFF"
